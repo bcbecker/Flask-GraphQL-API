333bcd952cb9338952a4d147e9b890801c300d9a
Bryce Becker <bryce.becker15@gmail.com>
2022-04-02 00:55:13 -0400

    Added create/update/delete mutations for Employee
    Added mutations for Employee. Need to break schema.py into multiple files

--- backend/api/schema.py
+++ backend/api/schema.py
@@ -1,6 +1,7 @@
 import graphene
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
+from bson.objectid import ObjectId
 from .models import (
     Department as DepartmentModel,
     Employee as EmployeeModel,
@@ -11,6 +12,7 @@ from .models import (
 '''
 TYPES
 '''
+
 
 class Department(MongoengineObjectType):
     class Meta:
@@ -39,25 +41,100 @@ class Employee(MongoengineObjectType):
 '''
 MUTATIONS
 '''
-#TODO: write mutations for Employee 
+
 
 class CreateEmployee(graphene.Mutation):
     employee = graphene.Field(Employee)
 
     class Arguments:
-        pass
+        name = graphene.String(required=True)
+        department_id = graphene.ID(required=True)
+        roles_id = graphene.List(graphene.ID, required=True)
+        manager_id = graphene.ID()
+        tasks = graphene.List(graphene.String)
+
+    def mutate(root, info, name, department_id, roles_id, manager_id=None, tasks=None):
+
+        _department = DepartmentModel.objects.get(id=ObjectId(department_id))
+        _roles = [RoleModel.objects.get(id=ObjectId(_role_id))
+                  for _role_id in roles_id]
+        _manager = EmployeeModel.objects.get(id=ObjectId(manager_id))
+
+        if tasks:
+            tasks = [TaskModel(name=_name) for _name in tasks]
+
+        _employee = EmployeeModel(
+            name=name,
+            department=_department,
+            manager=_manager,
+            roles=_roles,
+            tasks=tasks,
+        )
+
+        if _employee:
+            _employee.save()
+
+        return CreateEmployee(employee=_employee)
+
 
 class UpdateEmployee(graphene.Mutation):
     employee = graphene.Field(Employee)
 
     class Arguments:
-        pass
+        id = graphene.ID(required=True)
+        name = graphene.String()
+        department_id = graphene.ID()
+        roles_id = graphene.List(graphene.ID)
+        manager_id = graphene.ID()
+        tasks = graphene.List(graphene.String)
+
+    def mutate(root, info, id, name=None, department_id=None, roles_id=None, manager_id=None, tasks=None):
+
+        _employee = EmployeeModel.objects.get(id=ObjectId(id))
+
+        if name:
+            _employee.name = name
+
+        if department_id:
+            _department = DepartmentModel.objects.get(
+                id=ObjectId(department_id))
+
+            _employee.department = _department
+
+        if roles_id:
+            _roles = [RoleModel.objects.get(
+                id=ObjectId(_role_id)) for _role_id in roles_id]
+
+            _employee.roles = _roles
+
+        if manager_id:
+            _manager = EmployeeModel.objects.get(
+                id=ObjectId(manager_id))
+
+            _employee.manager = _manager
+
+        if tasks:
+            _tasks = [TaskModel(name=_name) for _name in tasks]
+
+            _employee.tasks = _tasks
+
+        if _employee:
+            _employee.save()
+
+        return UpdateEmployee(employee=_employee)
+
 
 class DeleteEmployee(graphene.Mutation):
     employee = graphene.Field(Employee)
 
     class Arguments:
-        pass
+        id = graphene.ID(required=True)
+
+    def mutate(root, info, id):
+        employee = EmployeeModel.objects.get(id=ObjectId(id))
+        employee.delete()
+
+        return DeleteEmployee(employee=employee)
 
 
 '''
@@ -89,11 +166,12 @@ class Mutation(graphene.ObjectType):
     create_employee = CreateEmployee.Field()
     update_employee = UpdateEmployee.Field()
     delete_employee = DeleteEmployee.Field()
-    
+
 
 '''
 SCHEMA
 '''
 
 
-schema = graphene.Schema(query=Query, types=[Department, Role, Task, Employee])
+schema = graphene.Schema(query=Query, mutation=Mutation, types=[
+                         Department, Role, Task, Employee])
